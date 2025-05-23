08c2d357bfe444c3021be1889cb1c8a0c2b93c99
Tboiii-123 <lawalhussein775@gmail.com>
2025-05-23 02:31:43 +0100

    commited

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_hg5a',
+        'USER': 'ecommerce_hg5a_user',
+        'PASSWORD': '7RIK0YAV3ElS9ahpMJPOAX36u5f6jcoY',
+        'HOST': 'dpg-d0nstjruibrs73c4s5qg-a.oregon-postgres.render.com',
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
