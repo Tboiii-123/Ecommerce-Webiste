91185a2725c1158ff3b5fe031737d1ffe7660446
Tboiii-123 <lawalhussein775@gmail.com>
2025-05-23 02:11:18 +0100

    commited

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -30,7 +30,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
-    path('__reload__/',include('django_browser_reload.urls'))
+    
 ]+static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT) 
 
 
