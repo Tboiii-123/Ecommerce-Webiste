7fe4018c9c2977143a27a90c8e1a9c77184fd8f2
Tboiii-123 <lawalhussein775@gmail.com>
2025-05-31 16:18:44 +0100

    commited

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login ,logout
+
 from django.contrib import messages
 from django.contrib.auth.models import User
+
 from .models import Profile,Product,Category,Cart, CartItem
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
